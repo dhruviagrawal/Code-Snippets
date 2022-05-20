d446ac5f5e9277f11e74cfeec8e64186b866ef4c
Dhruvi Agrawal <dhruviagrawal2@gmail.com>
2022-05-20 17:38:08 +0530

    20/05

---
+++ recursion/combinationSum1.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void func(int ind, vector<int>& ds, int sum, vector<int>& candidates, int target, vector<vector<int>>&ans)
+    {
+        int n=candidates.size();
+        if(ind>=n)
+            return;
+        
+        ds.push_back(candidates[ind]);
+        sum+=candidates[ind];
+        if(sum<target)
+            func(ind, ds, sum, candidates, target, ans);
+        if(sum==target)
+            ans.push_back(ds);
+        sum-=candidates[ind];
+        ds.pop_back();
+        func(ind+1, ds, sum, candidates, target, ans);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        func(0, ds, 0, candidates, target, ans);
+        return ans;
+    }
+};
